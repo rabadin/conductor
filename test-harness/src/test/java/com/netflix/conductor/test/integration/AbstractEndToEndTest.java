2b273cbd2386a389c4003f0da18649c404b0dad1
fthankac <fthankac@cisco.com>
2021-11-11 16:20:33 +0530

    CSCvz51898: ES7 support changes

--- test-harness/src/test/java/com/netflix/conductor/test/integration/AbstractEndToEndTest.java
+++ test-harness/src/test/java/com/netflix/conductor/test/integration/AbstractEndToEndTest.java
@@ -42,7 +42,7 @@ import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@TestPropertySource(properties = {"conductor.indexing.enabled=true", "conductor.elasticsearch.version=6"})
+@TestPropertySource(properties = {"conductor.indexing.enabled=true", "conductor.elasticsearch.version=7"})
 public abstract class AbstractEndToEndTest {
 
     private static final Logger log = LoggerFactory.getLogger(AbstractEndToEndTest.class);
@@ -55,7 +55,7 @@ public abstract class AbstractEndToEndTest {
 
     private static final ElasticsearchContainer container = new ElasticsearchContainer(DockerImageName
             .parse("docker.elastic.co/elasticsearch/elasticsearch-oss")
-            .withTag("6.8.12")); // this should match the client version
+            .withTag("7.6.2")); // this should match the client version
 
     private static RestClient restClient;
 
